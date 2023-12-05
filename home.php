43801e48b7829fe26ba3e171b45d37c46c12734a
Andhika Nur Afian <andhika.na@gmail.com>
2023-12-05 10:03:05 +0700

    05122023

--- home.php
+++ home.php
@@ -139,7 +139,7 @@
     </div>
 
     <div class="row">
-      <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+      <div class="col-md-6 col-lg-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
         <div class="icon-box">
           <div class="icon"><i class="bx bx-file"></i></div>
           <h4 class="title"><a href="">Surat Permohonan</a></h4>
@@ -147,7 +147,7 @@
         </div>
       </div>
 
-      <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+      <div class="col-md-6 col-lg-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
         <div class="icon-box">
           <div class="icon"><i class="bx bx-file"></i></div>
           <h4 class="title"><a href="">Formulir</a></h4>
